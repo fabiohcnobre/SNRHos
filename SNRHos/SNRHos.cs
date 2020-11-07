92c582b84cd347ec0506a503d8991d60f389163e
Fabio Henrique Cardoso Nobre <fabiohcnobre@hotmail.com>
2020-11-06 22:57:40 -0300

    Implementacao

---
+++ SNRHos/SNRHos.cs
@@ -0,0 +1,80 @@
+﻿using SNRHos.DFe.Utils;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SNRHos
+{
+    class SNRHos
+    {
+        public string ChaveAcesso { get; set; }
+        public string SnNum { get; set; }
+        public DateTime dataCheckin { get; set; }
+        public DateTime dataCheckout { get; set; }
+        public FNRH Fnrh { get; set; }
+
+        public string FnrhInserir()
+        {
+            //<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"
+            // xmlns: web = "http://webservice.ws.snrhos.id2.inf.br/" >
+
+            var message = new StringBuilder();
+            message.Append("<web:fnrhInserir>");
+            message.Append("<chaveAcesso>" + ChaveAcesso + "</chaveAcesso>");
+            message.Append(FuncoesXml.ClasseParaXmlString<FNRH>(Fnrh));
+            message.Append("</web:fnrhInserir>");
+
+            return SOAP.CallWebService(message.ToString());
+        }
+
+        public string FnrhAtualizar()
+        {
+            //<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"
+            // xmlns: web = "http://webservice.ws.snrhos.id2.inf.br/" >
+
+            var message = new StringBuilder();
+            message.Append("<web:fnrhAtualizar>");
+            message.Append("<chaveAcesso>" + ChaveAcesso + "</chaveAcesso>");
+            message.Append("<snNum>" + SnNum + "</snNum>");
+            message.Append(FuncoesXml.ClasseParaXmlString<FNRH>(Fnrh));
+            message.Append("</web:fnrhAtualizar>");
+
+            return SOAP.CallWebService(message.ToString());
+        }
+
+        public string FnrhCheckin()
+        {
+            //<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"
+            // xmlns: web = "http://webservice.ws.snrhos.id2.inf.br/" >
+
+            var message = new StringBuilder();
+            message.Append("<web:fnrhCheckin>");
+            message.Append("<chaveAcesso>" + ChaveAcesso + "</chaveAcesso>");
+            message.Append("<snNum>" + SnNum + "</snNum>");
+            message.Append("<dataCheckin>" + dataCheckin.ToString("aaaa-MM-ddTHH:mm:ss") + "</dataCheckin>");
+            message.Append("</web:fnrhCheckin>");
+
+
+            return SOAP.CallWebService(message.ToString());
+        }
+
+        public string FnrhCheckout()
+        {
+            //<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"
+            // xmlns: web = "http://webservice.ws.snrhos.id2.inf.br/" >
+
+            var message = new StringBuilder();
+            message.Append("<web:fnrhCheckout>");
+            message.Append("<chaveAcesso>" + ChaveAcesso + "</chaveAcesso>");
+            message.Append("<snNum>" + SnNum + "</snNum>");
+            message.Append("<dataCheckout>" + dataCheckout.ToString("aaaa-MM-ddTHH:mm:ss") + "</dataCheckout>");
+            message.Append("</web:fnrhCheckout>");
+
+
+            return SOAP.CallWebService(message.ToString());
+        }
+
+
+
+    }
+}
---
+++ SNRHos/SOAP.cs
@@ -0,0 +1,69 @@
+﻿
+using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace SNRHos
+{
+
+    public class SOAP
+    {
+        public static string CallWebService(string mensagem)
+        {
+            var _url = "http://fnrhws.hospedagem.turismo.gov.br/FnrhWs/FnrhWs?wsdl";
+            var _action = "http://fnrhws.hospedagem.turismo.gov.br/FnrhWs/FnrhWs?wsdl";
+
+            XmlDocument soapEnvelopeXml = CreateSoapEnvelope(mensagem);
+            HttpWebRequest webRequest = CreateWebRequest(_url, _action);
+            InsertSoapEnvelopeIntoWebRequest(soapEnvelopeXml, webRequest);
+
+            // begin async call to web request.
+            IAsyncResult asyncResult = webRequest.BeginGetResponse(null, null);
+
+            // suspend this thread until call is complete. You might want to
+            // do something usefull here like update your UI.
+            asyncResult.AsyncWaitHandle.WaitOne();
+
+            // get the response from the completed web request.
+            string soapResult = "";
+            using (WebResponse webResponse = webRequest.EndGetResponse(asyncResult))
+            {
+                using (StreamReader rd = new StreamReader(webResponse.GetResponseStream()))
+                {
+                    soapResult = rd.ReadToEnd();
+                }
+                Console.Write(soapResult);
+            }
+            return soapResult;
+        }
+
+        private static HttpWebRequest CreateWebRequest(string url, string action)
+        {
+            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(url);
+            webRequest.Headers.Add("SOAPAction", action);
+            webRequest.ContentType = "text/xml;charset=\"utf-8\"";
+            webRequest.Accept = "text/xml";
+            webRequest.Method = "POST";
+            return webRequest;
+        }
+
+        private static XmlDocument CreateSoapEnvelope(string mensagem)
+        {
+             XmlDocument soapEnvelopeDocument = new XmlDocument();
+            soapEnvelopeDocument.LoadXml(@"<soapenv:Envelope xmlns:soapenv=""http://schemas.xmlsoap.org/soap/envelope/"" xmlns:web=""http://webservice.ws.snrhos.id2.inf.br/""><soapenv:Header/><soapenv:Body>" +
+                                         mensagem + "</soapenv:Body></soapenv:Envelope>");
+            return soapEnvelopeDocument;
+        }
+
+        private static void InsertSoapEnvelopeIntoWebRequest(XmlDocument soapEnvelopeXml, HttpWebRequest webRequest)
+        {
+            using (Stream stream = webRequest.GetRequestStream())
+            {
+                soapEnvelopeXml.Save(stream);
+            }
+        }
+    }
+}
