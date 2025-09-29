2054ac5dbc987df0db941827f5c42177c4cb7758
Fabio Nobre <fabiohcnobre@hotmail.com>
2025-09-28 23:40:26 -0300

    Ajuste

--- SNRHos/obj/Release/netstandard2.1/SNRHos.AssemblyInfo.cs
+++ SNRHos/obj/Release/netstandard2.1/SNRHos.AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Reflection;
     "integração com o Sistema Nacional de Registro de Hóspedes - SNRHos por meio de W" +
     "eb Service.")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.2")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0.2+6a00199ec29df527d2aadab1737cb3be73b899f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0.2+be31e8659dd34b6831d09b126edeb5f2c04ad257")]
 [assembly: System.Reflection.AssemblyProductAttribute("SNRHos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SNRHos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.2")]
